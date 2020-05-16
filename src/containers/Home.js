1274df86863481bdbe5f8aa8e4ce32defc46442b
Aditya Dhawan <aditya.dhawan@popxo.com>
2020-05-16 13:04:59 +0530

    fixes

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -17,7 +17,7 @@ const Home = () => {
                 <ReactSlick />
             </SliderWrap>
             <h3>Upcoming online courses <span className="live"><span className="oval"></span>LIVE</span></h3>
-            <p className="upcoming-head">Learn from live teachers, not pre-recorded videos, in our intelligent virtual classrooms.  <br/> <img src={SkillIcon} alt="SkillIcon" /> = 21st century skills you’ll unlock <img src={CampCoin} alt="CampCoin" /> = coins you’ll earn for completion</p>
+            <p className="upcoming-head">Learn from live teachers, not pre-recorded videos, in our intelligent virtual classrooms.  <br/> <img src={SkillIcon} alt="SkillIcon" /> = 21st century skills you’ll unlock <img src={CampCoin} alt="CampCoin" />= coins you’ll earn for completion</p>
             <Container>
                 <Row>
                     {courses.map(course => <CoursesGrid {...course} />)}
